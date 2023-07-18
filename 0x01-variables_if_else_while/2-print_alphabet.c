c72a5533e9889c9049279feaf7cca62fa4bf7ed5
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-18 06:00:53 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabets in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	return (0);
+}

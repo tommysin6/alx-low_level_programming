7aef16de17b431d16fea4de33390ef211bd29b39
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-19 07:06:14 -0700

    Hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all numbers of base sixteen in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c = 0;
+	char a = 'a';
+
+	while (c <= 9)
+	{
+		if (c == 9)
+		{
+			putchar(a);
+		}
+		putchar(c + '0');
+	}
+	putchar('\n');
+}

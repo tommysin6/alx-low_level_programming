9d8db7f0e6a1ead9affd5865f3ea55f93a6c5326
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-19 08:03:27 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int i = 0;
-	int j = 1;
+	int j;
 
 	while (i <= 8)
 	{
+		j = i + 1
 		while (j <= 9)
 		{
 			putchar(i + '0');
-			putchar(j + '1');
+			putchar(j + '0');
 
 			if (i < 8 || j < 9)
 			{

3825b33c5b13dddf168b48769ce68aab37a08b5d
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-19 07:26:19 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible combinations of single digits
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(c + '0');
+		putchar(",");
+		putchar(" ");
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}

480d1ea172407b019afeb763032d42e426d11300
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-19 06:53:26 -0700

    Lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints lowercase alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}

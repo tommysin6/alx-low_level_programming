d1af8dbcca0e7c6db9f2bd890a57bc405f5a3f70
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-20 01:22:58 -0700

    Success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i < 7 || j < 8 || k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

f8e557e5fa73edebb31a837be30097ec320039a9
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-20 07:02:35 -0700

    Software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,7 +30,7 @@ int main(void)
 					putchar(c + '0');
 					putchar(d + '0');
 
-					if (a != 9 && b != 9 && c != 9 && d != 9)
+					if (!(a == 9 && b == 8 && c == 9 && d == 9))
 					{
 						putchar(',');
 						putchar(' ');

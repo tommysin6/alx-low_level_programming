e7ea20901353c058c14fbf34bf31ea982f2b6f38
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-24 09:59:36 -0700

    Time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Prints nine times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (c < 10)
+				{
+					_putchar(' ');
+				}
+
+				_putchar(c + '0');
+
+			}
+		}
+		_putchar('\n');
+	}
+
+}

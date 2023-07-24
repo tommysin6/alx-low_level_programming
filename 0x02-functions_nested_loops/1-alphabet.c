d836f33aa959199aa8746c44044bad1f961dbe96
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-07-24 07:53:00 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - A program that prints the alphabets in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}

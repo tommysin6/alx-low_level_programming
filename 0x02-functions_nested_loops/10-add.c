dd5837296abdb47a5ae232e278e2645cf3428fb7
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-01 05:20:18 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - A program that adds two integers together
+ *
+ * @n: character to check
+ *
+ * @m: Character to check
+ *
+ * Return: Addition
+ */
+
+int add(int n, int m)
+{
+	return(n + m);
+}

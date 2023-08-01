5a252389bb0565d15d0bcc15f5b9c140a771ccfc
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-01 05:09:22 -0700

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,26 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (a < 9)
+			if (b == 0)
 			{
-				_putchar(",");
-				_putchar(" ");
-				_putchar(" ");
+				_putchar(c + '0');
 			}
-			_putchar(c + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (c < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((c / 10) + '0');
+				}
+				_putchar((c % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

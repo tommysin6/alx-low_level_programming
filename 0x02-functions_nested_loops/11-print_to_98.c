2561b1135dfa8c3917d9f6fad2753228581b7d8a
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-01 05:49:01 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_to_98 - A program that prints natural numbers
+ *
+ * @n: Character to check
+ *
+ * Return: Print natural numbers
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}

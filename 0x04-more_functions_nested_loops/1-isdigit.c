8ae93a1ef37618d0d1edd06c472e21d582561fd0
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-03 15:05:37 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - A program that checks for a digit
+ *
+ * @c: Character to check
+ *
+ * Return: 1 if it is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 1 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

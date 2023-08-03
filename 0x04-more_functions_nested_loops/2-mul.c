1fe61f816d23d32a39e3af9b49dd18c30e1ffb13
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-03 15:14:08 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - A program that multiplies two integers together
+ *
+ * @a: The first integer
+ *
+ * @b: The second integer
+ *
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

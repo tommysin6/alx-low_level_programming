822493222fb6358a707c0f174c82d8201630179b
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-03 05:47:28 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints first fifty fibonacci numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c;
+	int d;
+
+	for (d = 2; d < 50; d++)
+	{
+		c = a + b;
+		printf(", %d", c);
+
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}

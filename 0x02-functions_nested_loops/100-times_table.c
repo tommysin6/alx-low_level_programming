f1f1b8c7d044b629522cf4e635cb4723413ae971
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-03 05:09:42 -0700

    Time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * print_cell- Prints individual cell
- *
- * @num: Value of cell
- * @col: Column number
- */
-
-void print_cell(int num, int col)
-{
-        if (col != 0)
-        {
-                _putchar(',');
-                _putchar(' ');
-
-                if (num < 100)
-                {
-                        _putchar(' ');
-                }
-        }
-
-        if (num < 10)
-        {
-                _putchar(num + '0');
-	}
-        else if (num < 100)
-        {
-                _putchar((num / 10) + '0');
-                _putchar((num % 10) + '0');
-        }
-        else
-        {
-                _putchar((num / 100) + '0');
-                _putchar(((num / 10) % 10) + '0');
-                _putchar((num % 10) + '0');
-        }
-
-/**
  * print_times_table - A program that the n times table
  *
  * @n: The character to check
@@ -55,7 +19,34 @@ void print_times_table(int n)
 			for (b = 0; b <= n; b++)
 			{
 				c =  a * b;
-				print_cell(c, b);
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else if (c < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}

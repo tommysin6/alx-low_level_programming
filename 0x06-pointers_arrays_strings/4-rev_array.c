03de5facad16f4eae3e96bf75aefd5eb97c129fd
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-05 03:35:39 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - A program reverses the content of an array of integers
+ *
+ * @a: The array to be reversed
+ *
+ * @n: The number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int begin = 0;
+	int stop = n - 1;
+	int temp;
+
+	while (begin < stop)
+	{
+		temp = a[begin];
+		a[begin] = a[stop];
+		a[stopd] = temp;
+		begin++;
+		stop--;
+	}
+}

98e519b9c3c8df1b3ce851845727657482a7187a
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-05 02:31:11 -0700

    Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - A program that prints half of a string
+ *
+ * @str: The input string.
+ */
+
+
+void puts_half(char *str)
+{
+	int check = 0,a
+
+	while (check >= 0)
+	{
+		if (str[check] == '\0')
+			break;
+		check++;
+	}
+	if (check % 2 == 1)
+	{
+		a = check / 2;
+	}
+	else
+	{
+		a = (check - 1) / 2;
+	}
+
+	for (a++; a < check; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}

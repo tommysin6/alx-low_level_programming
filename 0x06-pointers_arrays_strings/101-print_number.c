cd69af916230e565d19489b20c2d53fee4a3ece8
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-06 06:04:15 -0700

    Print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - A program that prints an integer
+ *
+ * @n: The integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int a, b, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a = n * -1;
+	}
+	else
+	{
+		a = n;
+	}
+
+	b = a;
+	count = 1;
+
+	while (b > 9)
+	{
+		b /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((a / count) % 10) + 48);
+	}
+}

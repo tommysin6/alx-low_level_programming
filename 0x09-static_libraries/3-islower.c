24e0134dc70aa0b9016b04ba93c29cd1620eb295
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-09 11:36:48 -0700

    Static

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+/**
+ * _islower - a function that checks for lowercase character.
+ *
+ * @c: collect alphabets
+ * Return: (1) if c is lower otherwise (0)
+ *
+ */
+int _islower(int c)
+{
+	return islower(c);
+}

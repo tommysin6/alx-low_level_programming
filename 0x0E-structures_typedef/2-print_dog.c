983dc58b70a434f61e25bcb8bc65c6936a3dabfd
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-08-16 02:05:38 -0700

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,9 @@ void print_dog(struct dog *d);
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 

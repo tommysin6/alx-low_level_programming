f1f30332c8f615f6353d3a81920ca7253ef235ca
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-10-25 06:31:17 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - A function that returns the square root of a number
+ *
+ * @n: The input number
+ *
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_help(n, 1));
+}
+
+/**
+ * sqrt_help - _sqrt_recursion helper
+ *
+ * @i: The input number
+ * @j: The iterator
+ *
+ * Return: The result
+ */
+
+int sqrt_help(int i, int j)
+{
+	if (j * j == i)
+		return (j);
+	if (j * j > i)
+		return (-1);
+
+	return (sqrt_help(i, j + 1));
+}
+

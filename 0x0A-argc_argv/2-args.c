b97fb598cc108d30ed0b6efaa4fcdeece56ea60d
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-10-27 03:58:32 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * @argc: The argument count
+ * @argv: The argument variables
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int length = argc;
+
+	for (int i = 0; i < length; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}

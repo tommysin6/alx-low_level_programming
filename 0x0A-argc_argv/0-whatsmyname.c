2896d9a5bb0b3f5c172453d02b618bc9a83f5046
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-10-27 03:41:28 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - The main function
+ * @argc: Argument count
+ * @argv: Argument variables
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv[0]);
+
+	return 0;
+}

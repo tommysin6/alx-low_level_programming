e07c8b9e92429681834db6be8b5efd937cc2fffe
Okusanya Amos Oluwatomisin <amosokusanya11@gmail.com>
2023-10-27 03:52:43 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * @argc: The argument count
+ * @argv: The argument variables
+ *
+ * Return: 0 (Succcess)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
